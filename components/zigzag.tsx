294cb878176d66d650eb0262257df06965637152
Nate Sesti <sestinj@gmail.com>
2023-06-29 14:21:54 -0700

    switch zigzag order

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
-import FeatImage01 from "@/public/images/edit.png";
-import FeatImage02 from "@/public/images/explain.png";
+import FeatImage01 from "@/public/images/explain.png";
+import FeatImage02 from "@/public/images/edit.png";
 import FeatImage03 from "@/public/images/generate.png";
 
 export default function Zigzag() {
@@ -30,9 +30,78 @@ export default function Zigzag() {
                 <Image
                   className="max-w-full mx-auto md:max-w-none h-auto"
                   src={FeatImage01}
-                  width={448}
+                  width={768}
                   // height={405}
                   alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <h3 className="h3 mb-3">Get possible explanations</h3>
+                  <div className="text-xl text-purple-600 mb-2">
+                    ————————————————————
+                  </div>
+                  <p className="text-xl text-gray-400 mb-4">
+                    Ask Continue about a part of your code to get another
+                    perspective
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>"/explain what might cause this error?"</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        "/explain what is the load_dot_env library name?"
+                      </span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        "/explain how do I find running process on port 8000?"
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage02}
+                  width={448}
+                  // height={405}
+                  alt="Features 02"
                 />
               </div>
               {/* Content */}
@@ -84,75 +153,6 @@ export default function Zigzag() {
                       </svg>
                       <span>
                         "/edit Use 'Union' instead of a vertical bar here"
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            {/* 2nd item */}
-            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
-              {/* Image */}
-              <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
-                data-aos="fade-up"
-              >
-                <Image
-                  className="max-w-full mx-auto md:max-w-none h-auto"
-                  src={FeatImage02}
-                  width={768}
-                  // height={405}
-                  alt="Features 02"
-                />
-              </div>
-              {/* Content */}
-              <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-                data-aos="fade-left"
-              >
-                <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <h3 className="h3 mb-3">Get possible explanations</h3>
-                  <div className="text-xl text-purple-600 mb-2">
-                    ————————————————————
-                  </div>
-                  <p className="text-xl text-gray-400 mb-4">
-                    Ask Continue about a part of your code to get another
-                    perspective
-                  </p>
-                  <ul className="text-lg text-gray-400 -mb-2">
-                    <li className="flex items-center mb-2">
-                      <svg
-                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
-                        viewBox="0 0 12 12"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>"/explain what might cause this error?"</span>
-                    </li>
-                    <li className="flex items-center mb-2">
-                      <svg
-                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
-                        viewBox="0 0 12 12"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>
-                        "/explain what is the load_dot_env library name?"
-                      </span>
-                    </li>
-                    <li className="flex items-center">
-                      <svg
-                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
-                        viewBox="0 0 12 12"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>
-                        "/explain how do I find running process on port 8000?"
                       </span>
                     </li>
                   </ul>
