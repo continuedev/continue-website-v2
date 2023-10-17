145fe2759925500b592d1c39c30d62f541a73846
Ty Dunn <ty@tydunn.com>
2023-10-17 09:28:58 -0400

    newsletter id

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -6,7 +6,7 @@ export default function Newsletter() {
 
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div id="newsletter" className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* CTA box */}
         <div
           className="relative bg-purple-600 py-10 px-8 md:py-16 md:px-12"
