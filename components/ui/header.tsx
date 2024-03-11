58342c90b3400981fa522f4d7f37ff30a8c770bc
Nate Sesti <sestinj@gmail.com>
2024-03-11 15:54:36 -0700

    ✨ hiring banner

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link";
 import Image from "next/image";
-import MobileMenu from "./mobile-menu";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="absolute w-full z-30">
+      <a href="https://www.ycombinator.com/companies/continue/jobs">
+        <div className="py-2 text-center pr-8 backdrop-blur-sm bg-white/10">
+          We are hiring! →{" "}
+        </div>
+      </a>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
           {/* Site branding */}
